20044b2d7de62f2c0b42d55d4ee747ff078071dd
Randriamananjara Nirinasoa Herilanto <nirinasoa.herilanto@gmail.com>
2023-11-22 15:27:35 +0300

    reexport paths

---
+++ src/components/section/index.ts
@@ -0,0 +1,7 @@
+export { default as Hero } from './Hero/Hero';
+export { default as PopularProducts } from './PopularProducts/PopularProducts';
+export { default as AboutUs } from './AboutUs/AboutUs';
+export { default as Services } from './Services/Services';
+export { default as SpecialOffer } from './SpecialOffer/SpecialOffer';
+export { default as CustomerReview } from './CustomerReview/CustomerReview';
+export { default as NewsLetter } from './NewsLetter/NewsLetter';
